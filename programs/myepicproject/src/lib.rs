b0d1898f199efdeb2071b0362d73c6ffa749efc8
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-03 00:45:51 +0100

    Add Deployment Update

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("HEbpuQwwgdkNYMzwo2QK5dDhQikwYK3MXz1xrx7LnZdZ");
 
 #[program]
 pub mod myepicproject {
